7d169bf2919a4082888b3ac991e0a81cdc044759
Freewind <nowindlee@gmail.com>
2020-11-04 16:19:40 +0800

    typescript-jest-react-testing-library-snapshot-test-demo

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-TypeScript React "react-testing-library" Demo
+TypeScript React "react-testing-library" Snapshot Test Demo
 =============================================
 
-如何使用jest和`react-testing-library`来测试react component的行为。
+可以使用snapshot的方式来生成一个component的html代码的快照，看到结构的变化。
 
-`react-testing-library`已经迁移到`@testing-library/react`
+可惜的是function之类没有体现出来，不知道有没有更好办法
 
 ```
 npm install
--- src/hello.test.tsx
+++ src/hello.test.tsx
@@ -6,11 +6,7 @@ import Hello from "./hello";
 describe('Hello', () => {
   test('handle onChange', () => {
     const mockOnChange = jest.fn()
-    const wrapper = render(<Hello name='typescript' onChange={mockOnChange}/>)
-    const inputNode = wrapper.container.querySelector('input')!
-
-    fireEvent.change(inputNode, {target: {value: 'react'}});
-
-    expect(mockOnChange).toHaveBeenCalledWith('react');
+    const {container} = render(<Hello name='typescript' onChange={mockOnChange}/>)
+    expect(container).toMatchSnapshot();
   });
 })
